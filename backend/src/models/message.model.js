1952c061a6aa21224388039ce1b30bdda78896b0
sashasutton <sashasuttons3@gmail.com>
2025-04-07 19:02:09 +0200

    message controler, message.model.js, message.route done

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,11 +1,22 @@
 import mongoose from 'mongoose';
 
-const messageSchema = new monggose.Schema({
+const messageSchema = new mongoose.Schema({
     senderId:{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
-
-    }
+        ref: 'User',
+        required: true
+    },
+    receiverId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text:{
+        type: String,
+    },
+    image:{
+        type: String,
+    },
 }, {timestamps: true});
 
 
